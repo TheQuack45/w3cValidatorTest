87ff147f674abc66f0acd18c8f0685ab3971eb44
Daniel Quackenbush <thequack45@gmail.com>
2016-04-10 20:28:19 -0400

    Current version, upload is not formatted properly

--- w3cValidatorTest/Program.cs
+++ w3cValidatorTest/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Web;
 
 namespace w3cValidatorTest
 {
@@ -10,6 +14,59 @@ namespace w3cValidatorTest
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("What website to make a request to?");
+            string inputUrl = Console.ReadLine();
+            Console.WriteLine("WebRequest body:");
+            string requestBody = Console.ReadLine();
+
+            string htmlRequestBody = WebUtility.HtmlEncode(requestBody);
+            /*UnicodeEncoding unicodeEncodedRequestBody = new UnicodeEncoding();
+            byte[] unicodeEncodedRequestBodyByteArr = unicodeEncodedRequestBody.GetBytes(htmlRequestBody);*/
+            //byte[] unicodeEncodedRequestBodyByteArr = Encoding.Unicode.GetBytes(htmlRequestBody);
+            byte[] unicodeEncodedRequestBodyByteArr = Encoding.Unicode.GetBytes(requestBody);
+
+            #region debug TODO: remove
+            Console.WriteLine("Debug:");
+            Console.WriteLine(System.Text.Encoding.Default.GetString(unicodeEncodedRequestBodyByteArr));
+            #endregion
+
+            HttpWebRequest pingReq = HttpWebRequest.CreateHttp(inputUrl);
+            #region POST header
+            pingReq.KeepAlive = true;
+            pingReq.Method = "POST";
+            string boundary = "----------------------------" + DateTime.Now.Ticks.ToString("x");
+            pingReq.ContentType = "multipart/form-data; boundary=" + boundary;
+            //pingReq.ContentLength = unicodeEncodedRequestBodyByteArr.Length;
+            #endregion
+
+            #region File addition
+            string headerTemplate = "--{0}\r\nContent-Disposition: form-data;\r\nContent-Type: text/html\r\n";
+            string header = String.Format(headerTemplate, boundary);
+            byte[] encodedHeaderByteArr = Encoding.Unicode.GetBytes(header);
+            Stream pingReqStream = pingReq.GetRequestStream();
+            pingReqStream.Write(encodedHeaderByteArr, 0, encodedHeaderByteArr.Length);
+            pingReqStream.Write(unicodeEncodedRequestBodyByteArr, 0, unicodeEncodedRequestBodyByteArr.Length);
+            byte[] newLineBytes = Encoding.Unicode.GetBytes("\r\n");
+            pingReqStream.Write(newLineBytes, 0, newLineBytes.Length);
+            #endregion
+
+            #region Input stream end
+            byte[] finalBytes = Encoding.Unicode.GetBytes("--" + boundary + "--");
+            pingReqStream.Write(finalBytes, 0, finalBytes.Length);
+            pingReqStream.Close();
+            #endregion
+
+            HttpWebResponse pingReqResponse = (HttpWebResponse)pingReq.GetResponse();
+            Stream pingReqResponseStream = pingReqResponse.GetResponseStream();
+            StreamReader streamRead = new StreamReader(pingReqResponseStream);
+            string pingReqResponseString = streamRead.ReadToEnd();
+            /*XmlDocument responseXml = new XmlDocument();
+            responseXml.LoadXml(pingReqResponseString);
+            XmlNode responseXmlChild = responseXml.ChildNodes[1];
+            Console.WriteLine(responseXmlChild.InnerText);*/
+            Console.WriteLine(pingReqResponseString);
+
+            Console.ReadKey();
         }
     }
 }
